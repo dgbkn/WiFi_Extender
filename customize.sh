98b1c15265540ff3c0ddf26b6a85d760314bac4f
gursewak10 <45232955+gursewak10@users.noreply.github.com>
2020-02-27 16:23:44 +0530

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -4,6 +4,6 @@ ui_print "Device: "$DEVICE
 
 ui_print " "
 
-if [ $DEVICE != "violet" ] && [ $DEVICE != "whyred" ]; then
+if [ $DEVICE != "violet" ] && [ $DEVICE != "whyred" ] && [ $DEVICE != "lavender" ]; then
   abort " => Device '"$DEVICE"' is not supported yet"
 fi
